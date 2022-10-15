4b8b989044227406e5250aa7f1af1788924acdc7
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-10-15 11:06:54 +0530

    import java.util.Scanner; class T{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         for(int i=1;i<=b;++i){             if (i%2==1)             System.out.println(a+" x "+i+" = "+(a*i));         }     } }

---
+++ Tables.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class T{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        for(int i=1;i<=b;++i){
+            if (i%2==1)
+            System.out.println(a+" x "+i+" = "+(a*i));
+        }
+    }
+}
