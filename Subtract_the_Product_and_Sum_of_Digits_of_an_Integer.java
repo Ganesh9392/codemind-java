74cbb5691aba22653585725d972596aeb2055939
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-10-16 15:41:47 +0530

    import java.util.Scanner; class L{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int p=1,r=0,d;         while(n != 0){             d=n%10;             n=n/10;             p=p*d;             r+=d;         }         System.out.println(p-r);     } }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class L{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int p=1,r=0,d;
+        while(n != 0){
+            d=n%10;
+            n=n/10;
+            p=p*d;
+            r+=d;
+        }
+        System.out.println(p-r);
+    }
+}
