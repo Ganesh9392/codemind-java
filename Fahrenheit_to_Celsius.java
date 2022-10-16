c1a9fa80f8f39f8342793fc2859f0e7d7eec3711
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-10-16 17:14:35 +0530

    import java.util.Scanner; class L{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         double n=s.nextDouble();         double r=0.0;         r=((n-32)*5)/9;         System.out.printf("%.2f",r);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class L{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        double n=s.nextDouble();
+        double r=0.0;
+        r=((n-32)*5)/9;
+        System.out.printf("%.2f",r);
+    }
+}
